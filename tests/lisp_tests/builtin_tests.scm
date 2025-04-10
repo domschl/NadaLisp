6665a7232ff5b48b2c70d878ccc975a4dd6c915c
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-04-10 20:36:13 +0200

    mess

--- tests/lisp_tests/builtin_tests.scm
+++ tests/lisp_tests/builtin_tests.scm
@@ -100,7 +100,7 @@
   (assert-equal (env-symbols) (lambda (syms) 
                                (member 'test-var syms)))
   ; Cannot easily test env-describe directly since it outputs to stdout
-  (undef 'test-var))
+  (undef 'test-var)
 ; CMakes CTest bails out on this without disabling global symbol lookup errors!
   (assert-equal (eval 'test-var 
                      (lambda () 'undefined)
