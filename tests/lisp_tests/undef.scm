b0e9a3462d586d64ce1ad2e20b7bab3d6e6e37c3
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-04-18 18:09:05 +0200

    more passing tests

--- tests/lisp_tests/undef.scm
+++ tests/lisp_tests/undef.scm
@@ -1,11 +1,12 @@
 ; ----- Environment Functions Tests -----
-(define-test "environment-functions"
-  (define test-var 42)
+(define test-var 42)
+(define-test "environment-functions-1"
   (assert-equal (env-symbols) (lambda (syms) 
-                               (member 'test-var syms)))
+                               (member 'test-var syms))))
   ; Cannot easily test env-describe directly since it outputs to stdout
-  (undef 'test-var)
-; CMakes CTest bails out on this without disabling global symbol lookup errors!
+(undef 'test-var)
+(define-test "environment-functions-1"
   (assert-equal (eval 'test-var 
                      (lambda () 'undefined)
                      (lambda (val) val)) 'undefined))
+
