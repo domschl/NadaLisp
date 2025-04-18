693582331f59fcf67f8e95f98eb4f4e963bd857d
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-04-18 07:07:36 +0200

    mdlrz

---
+++ nadalib_std/algebraic_parse.scm
@@ -0,0 +1,148 @@
+;; Example implementation for special constants
+(define pi-value 'pi)  ;; Symbolic representation
+(define e-value 'e)
+(define i-value 'i)
+
+;; All numbers in the core are rational, so this should just use number?
+(define rational? 
+  (lambda (expr)
+    (number? expr)))
+
+;; A fraction is a rational that's not an integer
+(define fraction? 
+  (lambda (expr)
+    (and (rational? expr) 
+         (not (integer? expr)))))
+
+;; Irrational numbers can only be represented symbolically
+(define irrational? 
+  (lambda (expr)
+    (or (eq? expr 'pi) 
+        (eq? expr 'e))))
+
+;; Complex numbers are represented by the symbol i or compound expressions
+(define complex? 
+  (lambda (expr)
+    (eq? expr 'i)))
+
+;; Updated constant? predicate to include all special numeric constants
+(define constant?
+  (lambda (expr)
+    (or (number? expr)       ;; Rational numbers
+        (irrational? expr)   ;; Symbolic irrational constants
+        (complex? expr))))   ;; Symbolic complex constants
+
+;; Add a variable? predicate to check if something is a variable
+(define variable?
+  (lambda (expr)
+    (and (symbol? expr) 
+         (not (irrational? expr))
+         (not (complex? expr)))))
+
+;; Operator precedence table
+(define op-precedence 
+  (lambda (op)
+    (cond
+      ((equal? op "+") 1)
+      ((equal? op "-") 1)
+      ((equal? op "*") 2)
+      ((equal? op "/") 2)
+      ((equal? op "^") 3)
+      (else 0))))
+
+;; Check if a token is an operator
+(define operator? 
+  (lambda (token)
+    (or (equal? token "+")
+        (equal? token "-")
+        (equal? token "*")
+        (equal? token "/")
+        (equal? token "^"))))
+
+;; Find matching closing parenthesis - recursive version
+(define find-matching-paren
+  (lambda (tokens start)
+    (define find-match
+      (lambda (pos depth)
+        (cond
+          ((>= pos (length tokens)) -1)
+          ((equal? (list-ref tokens pos) "(") 
+           (find-match (+ pos 1) (+ depth 1)))
+          ((equal? (list-ref tokens pos) ")")
+           (if (= depth 1) 
+               pos
+               (find-match (+ pos 1) (- depth 1))))
+          (else (find-match (+ pos 1) depth)))))
+    (find-match (+ start 1) 1)))
+
+;; Find the operator with lowest precedence - recursive version
+(define find-lowest-precedence-op
+  (lambda (tokens)
+    (define find-op
+      (lambda (pos lowest-idx lowest-prec paren-level)
+        (cond
+          ((>= pos (length tokens)) lowest-idx)
+          
+          ((equal? (list-ref tokens pos) "(")
+           (let ((match (find-matching-paren tokens pos)))
+             (if (> match pos)
+                 (find-op (+ match 1) lowest-idx lowest-prec paren-level)
+                 (find-op (+ pos 1) lowest-idx lowest-prec (+ paren-level 1)))))
+          
+          ((equal? (list-ref tokens pos) ")")
+           (find-op (+ pos 1) lowest-idx lowest-prec (- paren-level 1)))
+          
+          ((and (= paren-level 0)
+                (operator? (list-ref tokens pos))
+                (or (= lowest-idx -1)
+                    (<= (op-precedence (list-ref tokens pos)) lowest-prec)))
+           (find-op (+ pos 1) pos (op-precedence (list-ref tokens pos)) paren-level))
+          
+          (else (find-op (+ pos 1) lowest-idx lowest-prec paren-level)))))
+    (find-op 0 -1 999 0)))
+
+;; Update process-tokens to use eval-op
+(define process-tokens
+  (lambda (tokens)
+    (cond
+      ;; Empty expression
+      ((null? tokens) '())
+      
+      ;; Single token
+      ((= (length tokens) 1) 
+       (process-token (car tokens)))
+      
+      ;; Parenthesized expression
+      ((and (equal? (car tokens) "(")
+            (let ((closing (find-matching-paren tokens 0)))
+              (and (> closing 0) 
+                   (= closing (- (length tokens) 1)))))
+       (process-tokens (sublist tokens 1 (- (length tokens) 1))))
+      
+      ;; Process by operator precedence
+      (else
+        (let ((op-pos (find-lowest-precedence-op tokens)))
+          (if (>= op-pos 0)
+              (let ((op (list-ref tokens op-pos))
+                    (left (process-tokens (sublist tokens 0 op-pos)))
+                    (right (process-tokens (sublist tokens (+ op-pos 1) (length tokens)))))
+                (if (equal? op "^")
+                    (expt-op left right)  ; Special handling for exponentiation
+                    (list (string->symbol op) left right)))
+              tokens))))))
+
+;; Process a token into a value or symbol
+(define process-token
+  (lambda (token)
+    (if (string->number token)
+        (string->number token)
+        (string->symbol token))))
+
+;; Simple infix to prefix converter
+(define infix->prefix
+  (lambda (expr)
+    (if (not (string? expr))
+        (begin
+          (display "Error: infix->prefix requires a string argument\n")
+          '())
+        (process-tokens (tokenize-expr expr)))))
